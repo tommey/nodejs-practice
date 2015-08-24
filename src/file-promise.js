d3156a80c84fca8d87a1374765e6c1635048a498
Tamas Toth <tommey@tommey.hu>
2015-08-24 08:47:48 -0400

    Added file-promise with multiple promises

--- src/file-promise.js
+++ src/file-promise.js
@@ -1,17 +1,25 @@
 var fs = require('fs');
 
-var p = new Promise(function(resolve, reject){
-    fs.readFile('../package.json', 'utf-8', function(err, data){
-        if (err) {
-            return reject(err);
-        }
+function readFile(path) {
+    return new Promise(function (resolve, reject) {
+        fs.readFile(path, 'utf-8', function (err, data) {
+            if (err) {
+                return reject(err);
+            }
 
-        resolve(data);
+            resolve(data);
+        });
     });
-});
+}
 
-p.then(function(data) {
-    console.log(data);
-}).catch(function(err){
-    console.log(err);
-});
+readFile('../package.json')
+    .then(console.log)
+    .catch(console.log);
+
+Promise.all([
+    readFile('../apple'),
+    readFile('../banana'),
+    readFile('../peach')
+])
+    .then(console.log)
+    .catch(console.error);
